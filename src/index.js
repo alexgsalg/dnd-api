f6ec4a3385be688fa513d30fbd72d177e42ba37e
Alex  Salgado <alexgsalgado@gmail.com>
2022-03-06 13:20:05 -0300

    remove headers

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', '*');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', 'GET');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     return res.status(200),json();
   }
   next();
